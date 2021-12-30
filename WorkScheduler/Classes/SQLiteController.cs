1b6099a1e6e34e8f96003725cabd913c1a87f98c
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2021-12-30 22:57:17 +0100

    removed age from db table and from code

--- WorkScheduler/Classes/SQLiteController.cs
+++ WorkScheduler/Classes/SQLiteController.cs
@@ -30,22 +30,20 @@ namespace WorkScheduler.Classes
                     {
                         int id = reader.GetInt32(0);
                         string firstname = reader.GetString(1);
-                        string surname = reader.GetString(2);
-                        int age = reader.GetInt32(3);
-                        string birthdate = reader.GetString(4);
-                        string telephone = reader.GetString(5);
-                        string mobile = reader.GetString(6);
-                        string street = reader.GetString(7);
-                        string region = reader.GetString(8);
-                        int housenumber = reader.GetInt32(9);
-                        bool isActive = reader.GetBoolean(10);
+                        string surname = reader.GetString(2);                       
+                        string birthdate = reader.GetString(3);
+                        string telephone = reader.GetString(4);
+                        string mobile = reader.GetString(5);
+                        string street = reader.GetString(6);
+                        string region = reader.GetString(7);
+                        int housenumber = reader.GetInt32(8);
+                        bool isActive = reader.GetBoolean(9);
 
                         EmployeeModel employee = new EmployeeModel()
                         {
                             Id = id,
                             FirstName = firstname,
-                            Surname = surname,
-                            Age = age,
+                            Surname = surname,                           
                             Birthdate = birthdate,
                             TelephoneNumber = telephone,
                             MobileNumber = mobile,
@@ -70,12 +68,11 @@ namespace WorkScheduler.Classes
                 connection.Open();
 
                 SqliteCommand command = connection.CreateCommand();
-                command.CommandText = @"INSERT INTO Employees (firstname, surname, age, birthdate, telephonenumber, mobilenumber, street, region, housenumber, active)
-                    VALUES (@firstname,@surname,@age,@birthdate,@telephonenumber,@mobilenumber,@street,@region,@housenumber,@active);";
+                command.CommandText = @"INSERT INTO Employees (firstname, surname, birthdate, telephonenumber, mobilenumber, street, region, housenumber, active)
+                    VALUES (@firstname,@surname,@birthdate,@telephonenumber,@mobilenumber,@street,@region,@housenumber,@active);";
 
                 command.Parameters.AddWithValue("@firstname", employee.FirstName);
-                command.Parameters.AddWithValue("@surname", employee.Surname);
-                command.Parameters.AddWithValue("@age", employee.Age);
+                command.Parameters.AddWithValue("@surname", employee.Surname);              
                 command.Parameters.AddWithValue("@birthdate", employee.Birthdate);
                 command.Parameters.AddWithValue("@telephonenumber", employee.TelephoneNumber);
                 command.Parameters.AddWithValue("@mobilenumber", employee.MobileNumber);
